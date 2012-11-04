f9e295c64e40e310766acfb442a4a007a4b8bddb
Justin Cheung <jygsaw@gmail.com>
2012-11-04 02:05:27 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SatSite::Application.config.secret_token = '14073d5dfefe51d5c3528d7b3c3f26d9dfb0fffa8a6fdb731bd2729c3a1c83136f31ce7989989879d5bf2e1c6f649421ee1ace0c6fcc8761afa62057733ffa7c'
